eb35864e8e5e9ee260f8d35c8c7d157bc3c3c181
Sithu Myo Set <65828880+gitaccsithu@users.noreply.github.com>
2022-01-23 20:09:00 +0630

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,7 @@
 const api_key = "bTcJzGafJ6kHEXNcZvT0ReTsl3OddHQ3";
 
 const get_weather = async (key) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
     const location_key = `${key}?apikey=${api_key}`;
 
     const response = await fetch(base + location_key);
@@ -10,7 +10,7 @@ const get_weather = async (key) => {
 };
 
 const get_city = async (city) => {
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${api_key}&q=${city}`;
 
     const response = await fetch(base + query);
